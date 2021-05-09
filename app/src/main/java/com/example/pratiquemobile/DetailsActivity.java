7f591389448fca883781323e13d2d4de3aa2cb65
Naaima BEN KADOUR <naimabenkadour78@gmail.com>
2021-05-09 11:44:12 +0000

    TP2 ANDROID ACTIVITIES AND INTENTS

---
+++ app/src/main/java/com/example/pratiquemobile/DetailsActivity.java
@@ -0,0 +1,44 @@
+package com.example.pratiquemobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        String myName = intent.getStringExtra("Name");
+        Integer myHidden = intent.getIntExtra("hiddencopy", -1);
+        TextView nameTV =(TextView) findViewById(R.id.textView3);
+        nameTV.setText(myName);
+
+        Button btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+                setResult(RESULT_OK);
+                finish();
+            }
+
+
+
+
+
+        });
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pratiquemobile/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.pratiquemobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int APPEL_ACTIV2=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText myEditText = (EditText) findViewById(R.id.editText);
+                String text = myEditText.getText().toString();
+                Intent i = new Intent(view.getContext(), DetailsActivity.class);
+                i.putExtra("Name", text);
+                i.putExtra("Hiddencopy", 55);
+                startActivityForResult(i, APPEL_ACTIV2);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Toast toast = Toast.makeText(getApplicationContext(), "RETURN WITH A CANCEL", Toast.LENGTH_SHORT);
+            toast.setMargin(70, 50);
+            toast.show();
+        } else if (requestCode == APPEL_ACTIV2) {
+            TextView nameTV = (TextView) findViewById(R.id.textView);
+            nameTV.setText("LE NOM A ETE ENREGISTRE AVEC SUCCES!");
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "ERROR ERROR !", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+}
